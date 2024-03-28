6afb7ce7c8aaf63791dd194b0c550063b153a962
bryanathll <bryannatanael77@gmail.com>
2024-03-28 17:01:39 +0700

    InitialCommit

--- src/handle-rejection.js
+++ src/handle-rejection.js
@@ -1,22 +1,25 @@
 import winston from "winston"
 
-    const logger = winston.createLogger({
-        level: "debug",
-        // handleExceptions:true,
-        // handleRejections: true,
-        format: winston.format.combine(
-            winston.format.timestamp(),
-            winston.format.ms(),
-            winston.format.json(),
-        ),
-        transports: [
-            new winston.transports.Console({}),
-            new winston.transports.File({
-                handleExceptions:true,
-                handleRejections: true,
-                filename: "src/rejection.log"
-            })
-        ] 
-    });
+async function callAsync(){
+    return Promise.reject("ups");
+}
 
-    callAsync();
+const logger = winston.createLogger({
+    level: "debug",
+    // handleExceptions:true,
+    // handleRejections: true,
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.ms(),
+        winston.format.json(),
+    ),
+    transports: [
+        new winston.transports.Console({}),
+        new winston.transports.File({
+            handleRejections: true,
+            filename: "src/rejection.log"
+        })
+    ] 
+});
+
+callAsync();
