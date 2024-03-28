f6f5661cdcff5df9c15b945fb2c347cebd561e60
bryanathll <bryannatanael77@gmail.com>
2024-03-28 14:22:42 +0700

    InitialCommit

---
+++ test/logger-levels.test.js
@@ -0,0 +1,17 @@
+import winston from "winston"
+
+test("test logging with transport", ()=>{
+    const logger = winston.createLogger({
+        transports: [
+            new winston.transports.Console({})
+        ]
+    });
+
+    logger.log({level: "error", message: "hello error"})
+    logger.log({level: "warn", message: "hello warn"})
+    logger.log({level: "info", message: "hello logging"})
+    logger.log({level: "http", message: "hello http"})
+    logger.log({level: "verbose", message: "hello verbose"})
+    logger.log({level: "debug", message: "hello debug"})
+    logger.log({level: "silly", message: "hello silly"})
+})
